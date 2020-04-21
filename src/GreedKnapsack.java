ff4b5098cb9ea2d12b2c26a69dec028a4c0ef935
Arix23 <arivalenzuela23@gmail.com>
2020-04-21 17:48:50 -0600

    Divided into 2 parts

---
+++ src/GreedKnapsack.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class GreedKnapsack {
+	public static void main(String[] args) {
+		System.out.println("Hola amiguitos :D");
+	}
+	
+	
+	
+	
+	
+	public static void GreedyKnapSack(int objectNumber, ArrayList<Integer> objectValues, ArrayList<Integer> objectWeights, int maxWeight) {
+		
+	}
+	
+	
+	
+}
--- src/Knapsack.java
+++
@@ -1,6 +0,0 @@
-
-public class Knapsack {
-	public static void main(String[] args) {
-		System.out.println("Hola amiguitos :D");
-	}
-}
