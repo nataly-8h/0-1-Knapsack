2f4fb0a48db45f51e2be2567561a2a2fc4d55da0
Arix23 <arivalenzuela23@gmail.com>
2020-04-22 13:44:13 -0600

    Finished DP approach

--- src/DynKnapsack.java
+++ src/DynKnapsack.java
@@ -13,6 +13,7 @@ public class DynKnapsack {
 			objectWeights[i] = sc.nextInt();
 		}
 		int WeightMax = sc.nextInt();
+		DynamicKnapSack(objectNumber, objectValues, objectWeights, WeightMax);
 	}
 	
 	
@@ -20,6 +21,38 @@ public class DynKnapsack {
 	
 	
 	public static void DynamicKnapSack(int objectNumber, int[] objectValues, int[] objectWeights, int maxWeight) {
+		int maxValue = 0;
+		int[] objectsUsed = new int[objectNumber];
+		int[][] dynamicMatrix = new int[objectNumber][maxWeight+1];
+		for(int i = 0; i<=maxWeight; i++) {
+			if(i>=objectWeights[0]) {
+				dynamicMatrix[0][i] = objectValues[0];
+			} else {
+				dynamicMatrix[0][i] = 0;
+			}
+		}
+		if(objectNumber == 1) {
+			maxValue = dynamicMatrix[0][maxWeight];
+			objectsUsed[0] = 1;
+		} else {
+			for(int i = 1; i<objectNumber; i++) {
+				for(int j=0; j<=maxWeight; j++) {
+					if(j>=objectWeights[i]) {
+						System.out.println(j);
+						dynamicMatrix[i][j] = Math.max(objectValues[i]+dynamicMatrix[i-1][j-objectWeights[i]],dynamicMatrix[i-1][j]);
+					} else {
+						dynamicMatrix[i][j] = dynamicMatrix[i-1][j];
+					}
+				}
+			}
+		}
+		for(int i = 0; i<objectNumber; i++) {
+			System.out.println();
+			for(int j=0; j<=maxWeight; j++) {
+				System.out.print(dynamicMatrix[i][j]);
+				System.out.print(" ");
+			}
+		}
 		
 	}
 	
