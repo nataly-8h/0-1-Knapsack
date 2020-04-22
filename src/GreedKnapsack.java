5dab21c48b20feb67ddef3f16b098a9abc4826f7
nataly-8h <54066974+nataly-8h@users.noreply.github.com>
2020-04-21 20:23:14 -0500

    uuuu
    Quicksort en greedy

--- src/GreedKnapsack.java
+++ src/GreedKnapsack.java
@@ -1,10 +1,72 @@
 import java.util.*;
 
 public class GreedKnapsack {
+	private int max = 15;
+	
 	public static void main(String[] args) {
 		System.out.println("Hola amiguitos :D");
+		int [] profit = {10, 5, 15, 7, 6, 18, 3};
+		int[] weight = {2, 3, 5, 7, 1, 4, 1};
+		System.out.println(5/3);
+		knapsack(profit, weight);
+		
 	}
 	
+	/////////////QUICKSORT
+	public static<E extends Comparable<E>> void quicksort(E[] datos) {
+		swap(datos, 0, (int) Math.random()*datos.length);
+		quicksort(datos, 0,datos.length-1);
+	}
+	
+	private static<E extends Comparable<E>> void quicksort(E[] datos, int left, int right) {
+		if(left<right) {
+			int p = particionar(datos,left,right);
+			quicksort(datos,left, p-1);
+			quicksort(datos, p+1, right);
+		}
+	}
+	
+	private static<E extends Comparable<E>> int particionar(E[] datos, int left, int right) {
+		int i = left + 1;
+		for(int j = left+1; j<=right; j++) {
+			if(datos[j].compareTo(datos[left])<0) {
+				swap(datos, i, j);
+				i++;
+			}
+		}
+		swap(datos, left, i-1);
+		
+		return i-1;
+	}
+	
+	private static<E extends Comparable<E>> void swap(E[] valores, int i, int j) {
+		E aux = valores[j];
+		valores[j]=valores[i];
+		valores[i]=aux;
+		
+	}
+	
+	public static<E extends Comparable<E>> void imprimeArreglo(E[] valores) {
+		for(E valor:valores){
+			System.out.print(valor+"  ");
+		}
+		//System.out.println();
+	}
+	//////////////////////
+	
+	public static void knapsack(int[] profit, int[] weight){
+		Float[] relation = new Float[profit.length];
+		for(int i = 0; i<profit.length; i++) {
+			relation[i] = (float)profit[i]/weight[i];
+			System.out.print(relation[i] + " ");
+		}
+		System.out.println();
+		quicksort(relation);
+		imprimeArreglo(relation);
+		
+		
+		//return relation;
+	}
 	
 	
 	
