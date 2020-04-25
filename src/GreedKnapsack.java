f60c5a02d3fbbe7c748ab3b3afe8bdf6ffb43f87
Arix23 <arivalenzuela23@gmail.com>
2020-04-24 22:04:04 -0600

    bugfixes

--- src/GreedKnapsack.java
+++ src/GreedKnapsack.java
@@ -76,6 +76,7 @@ public class GreedKnapsack {
 	//////////////////////
 
 	public static void knapsack(int[] values, int[] weight, int maxWeight){
+		long startTime = System.nanoTime();
 		Items[] itemList = new Items[values.length];
 		int finalValue = 0;
 		String res = "";
@@ -104,6 +105,11 @@ public class GreedKnapsack {
 			System.out.println("El valor m�ximo es: " + finalValue);
 			System.out.println("Utilizando los objetos #: " + res);
 			System.out.println("Con los pesos: " + peso);
+			long endTime = System.nanoTime();
+			long timeElapsed = endTime - startTime;
+			System.out.println("Execution time in nanoseconds  : " + timeElapsed);
+			System.out.println("Execution time in milliseconds : " + timeElapsed / 1000000);
+					
 		}
 
 	}
